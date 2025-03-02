f8499ed5080cc115dbf7e0e77eee21500e36a7a1
phduckhoi <khoi76qn@gmail.com>
2025-03-02 23:01:47 +0700

    Add class Student

---
+++ practices/src/main/java/com/mycompany/practices/Student.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practices;
+
+/**
+ *
+ * @author khoi7
+ */
+public class Student {
+    private String studentId;
+    private String name;
+    private int age;
+
+    public Student(String studentId, String name, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getStudentId() { return studentId; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public void updateName(String newName) { this.name = newName; }
+}
